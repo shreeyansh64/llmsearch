f788ce4f6185f6a8ac868962d3513cbe7b9bb590
shreeyansh64 <shreeyansh64@gmail.com>
2026-01-05 04:13:58 +0530

    WebSearch : replaced search feature with baseTool

--- main.py
+++ main.py
@@ -3,26 +3,12 @@ from langchain_ollama import ChatOllama
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
 load_dotenv()
-tavily = TavilyClient()
-
-
-@tool
-def search(query : str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search result
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query=query)
 
 llm = ChatOllama(temperature=0, model="gpt-oss:20b")
-tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm,tools=tools)
 
 def main():
