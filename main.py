b7cfa6a276c0aa89c1e10e7aaf5b59b775a9a2e0
shreeyansh64 <shreeyansh64@gmail.com>
2026-01-05 04:09:47 +0530

    WebSearch: added websearch-tool

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+from dotenv import load_dotenv
+from langchain_ollama import ChatOllama
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from tavily import TavilyClient
+
+load_dotenv()
+tavily = TavilyClient()
+
+
+@tool
+def search(query : str) -> str:
+    """
+    Tool that searches over internet
+    Args:
+        query: The query to search for
+    Returns:
+        The search result
+    """
+    print(f"Searching for {query}")
+    return tavily.search(query=query)
+
+llm = ChatOllama(temperature=0, model="gpt-oss:20b")
+tools = [search]
+agent = create_agent(model=llm,tools=tools)
+
 def main():
-    print("Hello from llmsearch!")
+    print("Hello")
+    result = agent.invoke({"messages": HumanMessage(content="What is the weather in tokyo")})
+    print(result)
+    
 
 
 if __name__ == "__main__":
