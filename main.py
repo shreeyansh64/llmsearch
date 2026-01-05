1b7981fd9434d2aecae0160a9847e8e91dacde56
shreeyansh64 <shreeyansh64@gmail.com>
2026-01-05 21:32:40 +0530

    SearchTool : used lanchain_classic for Reasoning and action model

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from langchain_classic import hub
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
+from langchain_ollama import ChatOllama
+from dotenv import load_dotenv
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+
+tools = [TavilySearch()]
+
 def main():
     print("Hello from llmsearch!")
 
